3a3088aaa8e3e19a5bf18ac373a45115ab415ac4
Will Rutland <wrutland07@gmail.com>
2024-07-13 17:30:25 +0100

    bugfix

--- src/components/multiPage/scrollToTop.jsx
+++ src/components/multiPage/scrollToTop.jsx
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
 
+    const {pathname} = useLocation();
+
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, []);
+    }, [[pathname]]);
 
     return <></>;
 };
