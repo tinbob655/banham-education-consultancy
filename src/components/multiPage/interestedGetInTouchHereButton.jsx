52c0fb31e7ec2a557f10e4660fa7bce56bb5e38c
Will Rutland <wrutland07@gmail.com>
2024-07-18 16:16:15 +0100

    bugfix

--- src/components/multiPage/interestedGetInTouchHereButton.jsx
+++ src/components/multiPage/interestedGetInTouchHereButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function interestedGetInTouchHere() {
+export default function InterestedGetInTouchHere() {
     return (
         <a href="mailto:dalebanham@gmail.com">
             <h3>
