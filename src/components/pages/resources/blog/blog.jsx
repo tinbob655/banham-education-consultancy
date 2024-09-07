444b09403e10379222f9af974284c596237a52ef
Will Rutland <wrutland07@gmail.com>
2024-09-07 12:26:16 +0100

    added mobile resources page

--- src/components/pages/resources/blog/blog.jsx
+++ src/components/pages/resources/blog/blog.jsx
@@ -1,7 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {getDocs, query, collection, orderBy, getFirestore} from 'firebase/firestore';
+import { useIsMobile } from '../../../../context/mobileContext';
 
 export default function Blog() {
+
+    const isMobile = useIsMobile();
 
     function getInnerBlogContent(separatedBlogContent) {
         let innerHTML = [];
@@ -60,49 +63,80 @@ export default function Blog() {
 
             //after getting the data, format it into repeatable markup
             let tempBlogHTML = [];
-            tempBlog.forEach((blog) => {
 
-                //separate the blog into sections of paragraphs and links
-                const separatedBlog = blog.content.split('#');
+            //desktop blog section
+            if (!isMobile) {
+                tempBlog.forEach((blog) => {
+                    
+                    //separate the blog into sections of paragraphs and links
+                    const separatedBlog = blog.content.split('#');
+                    
+                    if (tempBlog.indexOf(blog) % 2 === 0) {
+    
+                        tempBlogHTML.push(
+                            <p style={{whiteSpace: 'pre-wrap', fontSize: '20px'}} className="alignLeft">
+                                {getInnerBlogContent(separatedBlog)}
+                            </p>
+                        )
+    
+                        tempBlogHTML.push(
+                            <React.Fragment>
+                                <p className="alignRight">
+                                    -Edited on {blog.dateEdited}
+                                </p>
+                            </React.Fragment>
+                        )  ;              
+                    }
+                    else {
+                        tempBlogHTML.push(
+                            <React.Fragment>
+                                <p className="alignRight" style={{whiteSpace: 'pre-warp'}}>
+                                    {blog.content}
+                                </p>
+                                <p className="alignLeft">
+                                    -Edited on {blog.dateEdited}
+                                </p>
+                            </React.Fragment>
+                        );
+                    };
+    
+                    //if this is not the last blog, add a soft divider line between blogs
+                    if (tempBlog.indexOf(blog)!== tempBlog.length - 1) {
+                        tempBlogHTML.push(
+                            <React.Fragment>
+                                <div className="dividerLine weakDivider"></div>
+                            </React.Fragment>
+                        );
+                    };
+                });
+            }
 
-                if (tempBlog.indexOf(blog) % 2 === 0) {
+            //mobile blog section
+            else {
+                tempBlog.forEach((blog) => {
 
-                    tempBlogHTML.push(
-                        <p style={{whiteSpace: 'pre-wrap', fontSize: '20px'}} className="alignLeft">
-                            {getInnerBlogContent(separatedBlog)}
-                        </p>
-                    )
+                    //separate the blog into sections of paragraphs and links
+                    const separatedBlog = blog.content.split('#');
 
                     tempBlogHTML.push(
                         <React.Fragment>
-                            <p className="alignRight">
-                                -Edited on {blog.dateEdited}
+                            <p style={{whiteSpace: 'pre-wrap', fontSize: '15px'}} className="alignLeft">
+                                {getInnerBlogContent(separatedBlog)}
                             </p>
-                        </React.Fragment>
-                    )  ;              
-                }
-                else {
-                    tempBlogHTML.push(
-                        <React.Fragment>
-                            <p className="alignRight" style={{whiteSpace: 'pre-warp'}}>
-                                {blog.content}
-                            </p>
-                            <p className="alignLeft">
+                            <p className="alignRight" style={{marginTop:'5px'}}>
                                 -Edited on {blog.dateEdited}
                             </p>
                         </React.Fragment>
                     );
-                };
 
-                //if this is not the last blog, add a soft divider line between blogs
-                if (tempBlog.indexOf(blog)!== tempBlog.length - 1) {
-                    tempBlogHTML.push(
-                        <React.Fragment>
+                    //if this is not the last blog, add a soft divider line below
+                    if (tempBlog.indexOf(blog) !== tempBlog.length -1) {
+                        tempBlogHTML.push(
                             <div className="dividerLine weakDivider"></div>
-                        </React.Fragment>
-                    );
-                };
-            });
+                        );
+                    };
+                });
+            };
 
             //return the html
             setBlogHTML(tempBlogHTML);
@@ -121,7 +155,7 @@ export default function Blog() {
             <div className="dividerLine"></div>
 
             {/*blog section*/}
-            <div style={{maxWidth: '60%'}}>
+            <div style={isMobile ? {width: '100%'} : {maxWidth: '60%'}}>
                 <h2>
                     Read all about it
                 </h2>
--- src/components/pages/resources/resources.jsx
+++ src/components/pages/resources/resources.jsx
@@ -2,11 +2,14 @@ import React, {useState, useEffect} from 'react';
 import Blog from './blog/blog.jsx';
 import {getFirestore, getDocs, collection, orderBy, query} from 'firebase/firestore';
 import SmartImage from '../../multiPage/smartImage.jsx';
+import {useIsMobile} from '../../../context/mobileContext.jsx';
 
 export default function Resources() {
 
     const [links, setLinks] = useState(null);
     const [linksHTML, setLinksHTML] = useState(<></>);
+
+    const isMobile = useIsMobile();
 
     //fetches the resources form firestore
     useEffect(() => {
@@ -27,71 +30,97 @@ export default function Resources() {
         //only fire if there are supplied links
         if (links && links.length > 0) {
             let tempLinksHTML = [];
-            links.forEach((link) => {
-                if (links.indexOf(link) % 2 === 0) {
-                    
-                    //text on left, image on right
-                    tempLinksHTML.push(
-                        <React.Fragment>
-                            <a href={link.href} target="_blank">
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <td style={{width: '60%'}}>
-                                                <h2 className='alignLeft' style={{fontSize: '35px'}}>
-                                                    -{link.name}
-                                                </h2>
-                                                <p className='alignLeft' style={{whiteSpace: 'pre-wrap'}}>
-                                                    {link.description}
-                                                </p>
-                                            </td>
-                                            <td>
-                                                <SmartImage imageClasses="mainImage" imageURL={link.imageURL} />
-                                            </td>
-                                        </tr>
-                                    </thead>
-                                </table>
-                            </a>
-                        </React.Fragment>
-                    );
-                }
-                else {
 
-                    //image on left, text on right
-                    tempLinksHTML.push(
-                        <React.Fragment>
-                            <a href={link.href} target='_blank'>
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <td>
-                                                <SmartImage imageClasses="mainImage" imageURL={link.imageURL} />
-                                            </td>
-                                            <td style={{width: '60%'}}>
-                                                <h2 className="alignRight" style={{fontSize: '35px'}}>
-                                                    -{link.name}
-                                                </h2>
-                                                <p className="alignRight">
-                                                    {link.description}
-                                                </p>
-                                            </td>
-                                        </tr>
-                                    </thead>
-                                </table>
-                            </a>
-                        </React.Fragment>
-                    );
-                };
+            //desktop links
+            if (!isMobile) {
+                links.forEach((link) => {
+                    if (links.indexOf(link) % 2 === 0) {
+                        
+                        //text on left, image on right
+                        tempLinksHTML.push(
+                            <React.Fragment>
+                                <a href={link.href} target="_blank">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <td style={{width: '60%'}}>
+                                                    <h2 className='alignLeft' style={{fontSize: '35px'}}>
+                                                        -{link.name}
+                                                    </h2>
+                                                    <p className='alignLeft' style={{whiteSpace: 'pre-wrap'}}>
+                                                        {link.description}
+                                                    </p>
+                                                </td>
+                                                <td>
+                                                    <SmartImage imageClasses="mainImage" imageURL={link.imageURL} />
+                                                </td>
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                </a>
+                            </React.Fragment>
+                        );
+                    }
+                    else {
+    
+                        //image on left, text on right
+                        tempLinksHTML.push(
+                            <React.Fragment>
+                                <a href={link.href} target='_blank'>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <td>
+                                                    <SmartImage imageClasses="mainImage" imageURL={link.imageURL} />
+                                                </td>
+                                                <td style={{width: '60%'}}>
+                                                    <h2 className="alignRight" style={{fontSize: '35px'}}>
+                                                        -{link.name}
+                                                    </h2>
+                                                    <p className="alignRight">
+                                                        {link.description}
+                                                    </p>
+                                                </td>
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                </a>
+                            </React.Fragment>
+                        );
+                    };
+    
+                    //if this is not the last link, add a weak divider line
+                    if (links.indexOf(link) < links.length -1) {
+                        tempLinksHTML.push(
+                            <React.Fragment>
+                                <div className="dividerLine weakDivider"></div>
+                            </React.Fragment>
+                        );
+                    };
+                });
+            }
 
-                //if this is not the last link, add a weak divider line
-                if (links.indexOf(link) < links.length -1) {
+            //mobile links
+            else {
+                links.forEach((link) => {
                     tempLinksHTML.push(
                         <React.Fragment>
                             <div className="dividerLine weakDivider"></div>
+
+                            <h2>
+                                {link.name}
+                            </h2>
+                            <p style={{whiteSpace: 'pre-wrap', fontSize: '15px', marginBottom: '10px'}} className="alignLeft">
+                                {link.description}
+                            </p>
+
+                            <a href={link.href} target='_blank'>
+                                <SmartImage imageClasses="mainImage" imageStyles={{border: 'none', maxHeight: '50vh', width: 'auto', maxWidth: '90%'}} imageURL={link.imageURL} />
+                            </a>
                         </React.Fragment>
                     );
-                };
-            });
+                });
+            };
 
             setLinksHTML(tempLinksHTML);
         }
@@ -107,25 +136,53 @@ export default function Resources() {
     }, [links]);
 
 
-    return(
-        <React.Fragment>
-            <Blog/>
-
-            <div className="dividerLine"></div>
-
-            {/*links section*/}
-            <div>
-                <h2>
-                    Recommended resources
-                </h2>
-                <p>
-                    There are many resources which we at Banham Education Consultancy would recommend to all students in order to enrich their studies. These are:
-                </p>
-
-                <div style={{maxWidth: '80%'}}>
-                    {linksHTML}
+    //desktop resources section
+    if (!isMobile) {
+        return(
+            <React.Fragment>
+                <Blog/>
+    
+                <div className="dividerLine"></div>
+    
+                {/*links section*/}
+                <div>
+                    <h2 style={{marginBottom: 0}}>
+                        Recommended resources
+                    </h2>
+                    <p>
+                        There are many resources which we at Banham Education Consultancy would recommend to all students in order to enrich their studies. These are:
+                    </p>
+    
+                    <div style={{maxWidth: '80%'}}>
+                        {linksHTML}
+                    </div>
                 </div>
-            </div>
-        </React.Fragment>
-    );
+            </React.Fragment>
+        );
+    }
+
+    //mobile resources section
+    else {
+        return (
+            <React.Fragment>
+                <Blog/>
+
+                <div className="dividerLine"></div>
+
+                {/*links section*/}
+                <div>
+                    <h1 style={{marginBottom: 0}}>
+                        Recommended resources
+                    </h1>
+                    <p>
+                        There are many resources which we at Banham Education Consultancy would recommend to all students in order to enrich their studies. These are:
+                    </p>
+
+                    <div>
+                        {linksHTML}
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
 };
