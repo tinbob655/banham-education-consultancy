ddb80ec1180b06e69cbc8604268e21acabfec376
Will Rutland <wrutland07@gmail.com>
2026-05-31 16:41:40 +0100

    made sections hover in when the user scrolls to them

--- src/components/general/GenericMarkupSection.tsx
+++ src/components/general/GenericMarkupSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {type RefObject, useEffect, useRef} from 'react';
 
 interface params {
     heading: string;
@@ -9,9 +9,31 @@ interface params {
 export default function GenericMarkupSection({heading, left, children}:params):React.ReactElement {
 
     const alignment:string = left ? "alignLeft" : "alignRight";
+    const ref:RefObject<HTMLElement|null> = useRef<HTMLElement>(null);
+
+    useEffect(() => {
+
+        //make sure we have our element
+        const el:HTMLElement|null = ref?.current;
+        if (!el) return;
+
+        //show elements when they appear in view
+        const observer = new IntersectionObserver(
+            ([entry]:IntersectionObserverEntry[]):void => {
+                if (entry.isIntersecting) {
+                    el.classList.add('visible');
+                    observer.disconnect(); //only trigger one time
+                }
+            },
+            { threshold: 0.15 }
+        );
+
+        observer.observe(el);
+        return () => observer.disconnect();
+    }, []);
 
     return (
-        <section className={alignment}>
+        <section className={alignment} ref={ref}>
             <h2 className={"sectionTitle"}>
                 {heading}
             </h2>
