fdd63087514360d79b650feceab6416dfbdd32cb
Will Rutland <wrutland07@gmail.com>
2026-05-31 16:23:11 +0100

    made window scroll to top on page change

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './scss/index.scss';
 import {BrowserRouter} from "react-router";
 import Header from "./components/general/header/Header.tsx";
 import AllRoutes from "./AllRoutes.tsx";
 import Footer from "./components/general/footer/Footer.tsx";
+import ScrollToTop from "./components/general/ScrollToTop.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+        <ScrollToTop/>
 
       <Header/>
 
