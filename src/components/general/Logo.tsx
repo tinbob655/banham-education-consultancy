5894ee85f82cd4c48a7849b96cd0f1277923ed4d
Will Rutland <wrutland07@gmail.com>
2026-05-31 15:40:22 +0100

    created footer

--- src/components/general/Logo.tsx
+++ src/components/general/Logo.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
 interface params {
-    icon?:boolean
+    icon?:boolean;
+    noBG?:boolean;
 }
 
-export default function Logo({icon}:params):React.ReactElement {
+export default function Logo({icon, noBG}:params):React.ReactElement {
+
+    //work our which image to use
+    let imageSrc:string;
+    if (icon) {
+        imageSrc = 'logoIcon.png';  //just the icon part of the logo
+    }
+    else {
+        if (noBG) {
+            imageSrc = 'logoFullNoBG.png';  //the entire logo but with no background
+        }
+        else {
+            imageSrc = 'logoFull.png';  //the normal logo
+        }
+    }
 
     return (
-        <img src={icon ? 'logoIcon.png' : 'logoFull.png'} alt={"Banham Education Consultancy logo"}/>
+        <img src={imageSrc} alt={"Banham Education Consultancy logo"}/>
     )
 }
