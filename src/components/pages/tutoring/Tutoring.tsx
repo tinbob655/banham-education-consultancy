8ff5918ac7e4477c5e5dcedc28f173910676d893
Will Rutland <wrutland07@gmail.com>
2026-05-31 15:50:45 +0100

    made tutoring page

--- src/components/pages/tutoring/Tutoring.tsx
+++ src/components/pages/tutoring/Tutoring.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PageHeader from "../../general/PageHeader.tsx";
 import GenericMarkupSection from "../../general/GenericMarkupSection.tsx";
+import List from "../../general/list/List.tsx";
+import InterestedButton from "../../general/InterestedButton.tsx";
 
 export default function Tutoring():React.ReactElement {
 
@@ -23,6 +25,33 @@ export default function Tutoring():React.ReactElement {
                     of subjects. Most parents and students buy into the full programme. This consists of 4 one-hour
                     tutoring sessions that cover the important areas that hold the key to exam success:
                 </p>
+                <List items={[
+                    'Motivation and goal setting',
+                    'Booting your memory and effective revision strategies',
+                    'Developing effective study routines',
+                    'Taking responsibility for your learning',
+                ]}/>
+                <InterestedButton/>
+            </GenericMarkupSection>
+
+            <GenericMarkupSection heading={"Expert subject specific tutoring"} left>
+                <p>
+                    We are confident that our elite level tutoring sessions will significantly improve results for most
+                    students. However, sometimes students and their parents may be looking to improve their exam
+                    performance in one particular subject. We offer a team of subject experts to help improve results
+                    in a range of GCSE and A level subjects. We specialise in English, Maths, History, French, Spanish,
+                    Psychology and Law.
+                </p>
+                <List items={[
+                    `All of our tutors use the key memory and study techniques that are delivered in our elite level 
+                    tutoring programme so that the subject specific tutoring can build on tried and tested methods 
+                    for learning effectively.`,
+
+                    ` All of our tutors are experienced teachers who have either been Heads of Department or Examiners.`,
+
+                    `All of our tutors can offer tutoring online if parents/students prefer this approach.`
+                ]}/>
+                <InterestedButton/>
             </GenericMarkupSection>
         </React.Fragment>
     )
