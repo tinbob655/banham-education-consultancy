14d640802fdcdaa4138bbc1b3c554f207f402a58
Will Rutland <wrutland07@gmail.com>
2026-06-08 18:35:29 +0100

    organised public folder

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,14 +10,14 @@ export default function Logo({icon, noBG}:params):React.ReactElement {
     //work our which image to use
     let imageSrc:string;
     if (icon) {
-        imageSrc = 'logoIcon.png';  //just the icon part of the logo
+        imageSrc = 'logo/logoIcon.png';  //just the icon part of the logo
     }
     else {
         if (noBG) {
-            imageSrc = 'logoFullNoBG.png';  //the entire logo but with no background
+            imageSrc = 'logo/logoFullNoBG.png';  //the entire logo but with no background
         }
         else {
-            imageSrc = 'logoFull.png';  //the normal logo
+            imageSrc = 'logo/logoFull.png';  //the normal logo
         }
     }
 
