461ac8d7e4df3c0952257bb9a5a5c974127e4b17
Will Rutland <wrutland07@gmail.com>
2026-06-08 18:29:02 +0100

    added interested button to bottom of account page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -3,6 +3,7 @@ import PageHeader from "../../components/PageHeader.tsx";
 import staffData from './staffData.json' with {type: 'json'}
 import type {staffMember} from "./staffMember";
 import GenericMarkupSection from "../../components/GenericMarkupSection.tsx";
+import InterestedButton from "../../components/InterestedButton.tsx";
 
 export default function About():React.ReactElement {
 
@@ -11,13 +12,18 @@ export default function About():React.ReactElement {
             <PageHeader title={"About"} subtitle={"Experts in education"} />
 
             {/*transform staff data into markup*/}
-            {staffData.map((staffMember: staffMember):React.ReactElement => {
+            {staffData.map((staffMember: staffMember, index:number):React.ReactElement => {
+
+                const last:boolean = index === staffData.length - 1;
                 return (
                     <GenericMarkupSection heading={`${staffMember.name} ${staffMember.qualifications ?? ''}`}>
                         <span className={"tag"}>{staffMember.subject}</span>
                         <p style={{whiteSpace: 'pre-wrap'}}>
                             {staffMember.description}
                         </p>
+
+                        {/*interested button on last staff member*/}
+                        {last && <InterestedButton/>}
                     </GenericMarkupSection>
 
                 )
