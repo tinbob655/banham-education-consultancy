b66d5b57b7c401475f92c8b51366769de6100521
Will Rutland <wrutland07@gmail.com>
2026-06-08 18:55:41 +0100

    added images to about section

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -4,6 +4,7 @@ import staffData from './staffData.json' with {type: 'json'}
 import type {staffMember} from "./staffMember";
 import GenericMarkupSection from "../../components/GenericMarkupSection.tsx";
 import InterestedButton from "../../components/InterestedButton.tsx";
+import './aboutStyles.scss';
 
 export default function About():React.ReactElement {
 
@@ -16,11 +17,25 @@ export default function About():React.ReactElement {
 
                 const last:boolean = index === staffData.length - 1;
                 return (
-                    <GenericMarkupSection heading={`${staffMember.name} ${staffMember.qualifications ?? ''}`}>
-                        <span className={"tag"}>{staffMember.subject}</span>
-                        <p style={{whiteSpace: 'pre-wrap'}}>
-                            {staffMember.description}
-                        </p>
+                    <GenericMarkupSection heading={`${staffMember.name}: ${staffMember.qualifications ?? ''}`}>
+                        <div className={"staffMemberWrapper"}>
+
+                            {/*description*/}
+                            <div className={"aboutDescriptionWrapper"}>
+                                <span className={"tag"}>{staffMember.subject}</span>
+                                <p style={{whiteSpace: 'pre-wrap'}}>
+                                    {staffMember.description}
+                                </p>
+                            </div>
+
+                            {/*profile picture*/}
+                            <div className={"aboutProfilePictureWrapper"}>
+                                <img className={"profilePicture"} src={staffMember.profileImagePath} alt={`Image of${staffMember.name}`} />
+                                <span className={"tag"}>
+                                    {staffMember.name}
+                                </span>
+                            </div>
+                        </div>
 
                         {/*interested button on last staff member*/}
                         {last && <InterestedButton/>}
