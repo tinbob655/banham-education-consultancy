822693e5a17e63a738f49d8fe3b1fb78ea9b5e95
Will Rutland <wrutland07@gmail.com>
2026-06-13 11:47:28 +0100

    crunched down the build size

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,33 @@
 import {defineConfig} from 'vite'
 import react, {reactCompilerPreset} from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import {visualizer} from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
+
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    visualizer({open: true}),
+    babel({ presets: [reactCompilerPreset()] }),
   ],
+
   server: {
     port: 3000,
   },
+
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return 'vendor';
+        manualChunks(id: string) {
+          if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
+            return 'vendor-react';
+          }
+          if (id.includes('firebase')) {
+            return 'vendor-firebase';
           }
         }
       }
     }
-  }
+  },
 })
